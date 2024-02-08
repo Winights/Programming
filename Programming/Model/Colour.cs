17ff913fd991c1e2d7cffde8f1f63d8cc6be1bc2
Winights <fgfgfygty@gmail.com>
2024-02-08 21:19:39 +0700

    Новые изменения

---
+++ Programming/Model/Colour.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.Model
+{
+    public enum Colour
+    {
+        Red,
+        Green,
+        Blue,
+        Yellow,
+        Black,
+        White,
+        Orange,
+    }
+}
