13ad31c76c61463565cfaae61f28b25062901492
Winights <fgfgfygty@gmail.com>
2024-02-08 21:47:49 +0700

    Изменение формы

--- Programming/View/MainForm.Designer.cs
+++ Programming/View/MainForm.Designer.cs
@@ -28,12 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            tabControl1 = new TabControl();
+            Enums = new TabPage();
+            EnumsListBox = new ListBox();
+            ValuesListBox = new ListBox();
+            EnumsLabel = new Label();
+            ValueLabel = new Label();
+            Enumaration = new Label();
+            ValueTextBox = new TextBox();
+            TextBoxLabel = new Label();
+            tabControl1.SuspendLayout();
+            Enums.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(Enums);
+            tabControl1.Location = new Point(12, 12);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(776, 415);
+            tabControl1.TabIndex = 0;
+            // 
+            // Enums
+            // 
+            Enums.Controls.Add(TextBoxLabel);
+            Enums.Controls.Add(ValueTextBox);
+            Enums.Controls.Add(Enumaration);
+            Enums.Controls.Add(ValueLabel);
+            Enums.Controls.Add(EnumsLabel);
+            Enums.Controls.Add(ValuesListBox);
+            Enums.Controls.Add(EnumsListBox);
+            Enums.Location = new Point(4, 29);
+            Enums.Name = "Enums";
+            Enums.Padding = new Padding(3);
+            Enums.Size = new Size(768, 382);
+            Enums.TabIndex = 0;
+            Enums.Text = "Enums";
+            Enums.UseVisualStyleBackColor = true;
+            // 
+            // EnumsListBox
+            // 
+            EnumsListBox.FormattingEnabled = true;
+            EnumsListBox.Location = new Point(15, 53);
+            EnumsListBox.Name = "EnumsListBox";
+            EnumsListBox.Size = new Size(150, 224);
+            EnumsListBox.TabIndex = 0;
+            // 
+            // ValuesListBox
+            // 
+            ValuesListBox.FormattingEnabled = true;
+            ValuesListBox.Location = new Point(193, 53);
+            ValuesListBox.Name = "ValuesListBox";
+            ValuesListBox.Size = new Size(150, 224);
+            ValuesListBox.TabIndex = 1;
+            // 
+            // EnumsLabel
+            // 
+            EnumsLabel.AutoSize = true;
+            EnumsLabel.Location = new Point(15, 30);
+            EnumsLabel.Name = "EnumsLabel";
+            EnumsLabel.Size = new Size(146, 20);
+            EnumsLabel.TabIndex = 2;
+            EnumsLabel.Text = "Choose enumaration";
+            EnumsLabel.Click += this.label1_Click;
+            // 
+            // ValueLabel
+            // 
+            ValueLabel.AutoSize = true;
+            ValueLabel.Location = new Point(193, 30);
+            ValueLabel.Name = "ValueLabel";
+            ValueLabel.Size = new Size(97, 20);
+            ValueLabel.TabIndex = 3;
+            ValueLabel.Text = "Choose value";
+            // 
+            // Enumaration
+            // 
+            Enumaration.AutoSize = true;
+            Enumaration.Location = new Point(6, 3);
+            Enumaration.Name = "Enumaration";
+            Enumaration.Size = new Size(93, 20);
+            Enumaration.TabIndex = 4;
+            Enumaration.Text = "Enumaration";
+            // 
+            // ValueTextBox
+            // 
+            ValueTextBox.Location = new Point(367, 53);
+            ValueTextBox.Name = "ValueTextBox";
+            ValueTextBox.Size = new Size(169, 27);
+            ValueTextBox.TabIndex = 5;
+            // 
+            // TextBoxLabel
+            // 
+            TextBoxLabel.AutoSize = true;
+            TextBoxLabel.Location = new Point(367, 30);
+            TextBoxLabel.Name = "TextBoxLabel";
+            TextBoxLabel.Size = new Size(65, 20);
+            TextBoxLabel.TabIndex = 6;
+            TextBoxLabel.Text = "int value";
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 505);
+            Controls.Add(tabControl1);
+            Name = "MainForm";
+            Text = "Form1";
+            tabControl1.ResumeLayout(false);
+            Enums.ResumeLayout(false);
+            Enums.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private TabControl tabControl1;
+        private TabPage Enums;
+        private ListBox ValuesListBox;
+        private ListBox EnumsListBox;
+        private Label EnumsLabel;
+        private Label Enumaration;
+        private Label ValueLabel;
+        private Label TextBoxLabel;
+        private TextBox ValueTextBox;
     }
 }
