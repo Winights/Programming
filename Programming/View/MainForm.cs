68ee44f8323ce3477e9531131ab54eda247d8bb6
Winights <fgfgfygty@gmail.com>
2024-02-11 15:05:00 +0700

    Вторая лабораторная работа

--- Programming/View/MainForm.cs
+++ Programming/View/MainForm.cs
@@ -1,4 +1,4 @@
-using Programming.Model;
+using Programming.Model.Enums;
 using System.Windows.Forms;
 
 namespace Programming
@@ -10,6 +10,7 @@ namespace Programming
             InitializeComponent();
             PopulateEnumsListBox();
             DefaultSelectedEnums();
+            LoadSeasonComboBox();
         }
         private void PopulateEnumsListBox()
         {
@@ -30,9 +31,22 @@ namespace Programming
             EnumsListBox.SetSelected(0, true);
             ValuesListBox.SetSelected(1, true);
         }
+        private void LoadSeasonComboBox()
+        {
+            foreach (var item in Enum.GetValues(typeof(Season)))
+            {
+                SeasonComboBox.Items.Add(item);
+            }
+        }
         private void EnumsListBox_SelectedIndexChanged(object sender, EventArgs e)
         {
             ValuesListBox.Items.Clear();
+
+            if (EnumsListBox.SelectedItem == null)
+            {
+                MessageBox.Show("Выберите элемент");
+                return;
+            }
 
             //Название выбранного элемента в EnumsListBox
             string selectedEnumName = EnumsListBox.SelectedItem.ToString();
@@ -68,6 +82,67 @@ namespace Programming
             {
                 int selectedEnumName = ValuesListBox.SelectedIndex;
                 ValueTextBox.AppendText(selectedEnumName.ToString());
+            }
+        }
+        private void ParseButton_Click(object sender, EventArgs e)
+        {
+            ResultTextBox.Clear();
+
+            if (WeekdayTextBox.Text == "")
+            {
+                MessageBox.Show("Введите текст");
+                return;
+            }
+
+            string inputUserText = WeekdayTextBox.Text;
+            if (int.TryParse(inputUserText, out int intUserText) == true)
+            {
+                int IndexWeekday = intUserText - 1;
+                if (Enum.TryParse(IndexWeekday.ToString(), out Weekday valueWeekday) && (intUserText < 8))
+                {
+                    ResultTextBox.AppendText($"Это день недели ({intUserText} = {valueWeekday})");
+                }
+                else
+                {
+                    ResultTextBox.AppendText("Нет такого дня недели");
+                }
+            }
+
+            else
+            {
+                if (Enum.TryParse(inputUserText, out Weekday result) == true)
+                {
+                    int numericValue = (int)result;
+                    ResultTextBox.AppendText($"Это день недели ({inputUserText} = {numericValue + 1})");
+                }
+                else
+                {
+                    ResultTextBox.AppendText("Нет такого дня недели");
+                }
+            }
+        }
+        private void GoButton_Click(object sender, EventArgs e)
+        {
+            if (SeasonComboBox.SelectedItem == null)
+            {
+                MessageBox.Show("Выберите время года");
+                return;
+            }
+            Season selectedSeason = (Season)SeasonComboBox.SelectedItem;
+            switch (selectedSeason)
+            {
+                case Season.Summer:
+                    MessageBox.Show("Ура! Солнце!");
+                    break;
+                case Season.Autumn:
+                    this.BackColor = Color.Orange;
+                    break;
+                case Season.Winter:
+                    MessageBox.Show("Брр! Холодно!");
+                    break;
+                case Season.Spring:
+                    this.BackColor = Color.ForestGreen;
+                    break;
             }
         }
     }
