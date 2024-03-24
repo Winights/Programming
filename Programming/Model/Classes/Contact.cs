f67ad56989c079819146050be7341e401a4eb77e
Winights <fgfgfygty@gmail.com>
2024-03-24 20:28:49 +0700

    Лаба4 начало

--- Programming/Model/Classes/Contact.cs
+++ Programming/Model/Classes/Contact.cs
@@ -5,24 +5,61 @@ using System.Text;
 using System.Threading.Tasks;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 using System.Xml.Linq;
+using System.Text.RegularExpressions;
 
 namespace Programming.Model.Classes
 {
     public class Contact
     {
-        public string Name { get; set; }
+        private string _name;
+        private string _surname;
+        public string Name 
+        { 
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                AssertStringContainsOnlyLetters(value);
+                _name = value;
+            }
+        }
+        public string Surname
+        {
+            get
+            {
+                return _surname;
+            }
+            set
+            {
+                AssertStringContainsOnlyLetters(value);
+                _surname = value;
+            }
+        }
+        private void AssertStringContainsOnlyLetters(string value)
+        {
+            bool consistsOfEnglishLetters = Regex.IsMatch(value, @"^[a-zA-Z]+$");
+            if (consistsOfEnglishLetters == false)
+            {
+                throw new ArgumentException("Строка должна содержать только символы английского алфавита.");
+            }
+        }
+
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
 
-        public Contact(string name, string phoneNumber, string email)
+        public Contact(string name, string surname, string phoneNumber, string email)
         {
             Name = name;
+            Surname = surname;
             PhoneNumber = phoneNumber;
             Email = email;
         }
         public Contact()
         {
             Name = "";
+            Surname = "";
             PhoneNumber = "";
             Email = "";
         }
