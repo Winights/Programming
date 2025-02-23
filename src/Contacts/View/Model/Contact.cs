c7328ae2c39e0397cb0adcdb91ea5cef9064c490
Winights <fgfgfygty@gmail.com>
2025-02-23 21:27:40 +0700

    feature<Contact, ValueValidator>; добавление новых классов, а именно Contact и ValueValidator

---
+++ src/Contacts/View/Model/Contact.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using View.Model.Services;
+
+namespace View.Model
+{
+    /// <summary>
+    /// Хранит данные о контакте.
+    /// </summary>
+    public class Contact
+    {
+        /// <summary>
+        /// Полное имя контакта.
+        /// </summary>
+        private string _fullname = string.Empty;
+
+        /// <summary>
+        /// Почта контакта.
+        /// </summary>
+        private string _email = string.Empty;
+
+        /// <summary>
+        /// Номер телефона контакта.
+        /// </summary>
+        private string _phoneNumber = string.Empty;
+
+        /// <summary>
+        /// Возвращает и задает полное имя контакта. Должен не превышать 200 символов.
+        /// </summary>
+        public string Fullname
+        {
+            get
+            {
+                return _fullname;
+            }
+            set
+            {
+                ValueValidator.AssertStringOnLength(value, 200, "Name");
+                _fullname = value;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задает почту контакта. Должен не превышать 250 символов.
+        /// </summary>
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                ValueValidator.AssertStringOnPhoneEmail(value, "Email");
+                ValueValidator.AssertStringOnLength(value, 250, "Email");
+                _email = value;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задает номер телефона контакта. Должен быть в формате номера телефона.
+        /// </summary>
+        public string PhoneNumber
+        {
+            get
+            {
+                return _phoneNumber;
+            }
+            set
+            {
+                ValueValidator.AssertStringOnPhoneNumber(value, "PhoneNumber");
+                _phoneNumber = value;
+            }
+        }
+
+        /// <summary>
+        /// Создаёт экземпляр класса <see cref="Contact"/>.
+        /// </summary>
+        /// <param name="fullname">Полное имя контакта.</param>
+        /// <param name="email">Почта контакта.</param>
+        /// <param name="phoneNumber">Номер телефона контакта.</param>
+        public Contact(string fullname, string email, string phoneNumber)
+        {
+            Fullname = fullname;
+            Email = email;
+            PhoneNumber = phoneNumber;
+        }
+
+        /// <summary>
+        /// Создаёт пустой экземпляр класса <see cref="Contact"/>.
+        /// </summary>
+        public Contact()
+        {
+            Fullname = string.Empty;
+            Email = "yuri.smirnov@no.mail";
+            PhoneNumber = "+7-999-999-99-99";
+        }
+    }
+}
---
+++ src/Contacts/View/Model/Services/ValueValidator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace View.Model.Services
+{
+    public class ValueValidator
+    {
+        /// <summary>
+        /// Проверяет, что строка не больше заданного числа.
+        /// </summary>
+        /// <param name="value">Проверямая строка.</param>
+        /// <param name="max">Верхняя граница.</param>
+        /// <param name="propertyName">Имя свойства или объекта, которое подлежит проверке.</param>
+        public static void AssertStringOnLength(string value, int max, string propertyName)
+        {
+            if (value.Length > max)
+            {
+                throw new ArgumentOutOfRangeException($"Ошибка в {propertyName}");
+            }
+        }
+
+        /// <summary>
+        /// Проверяет, что строка по формату является номером телефона.
+        /// </summary>
+        /// <param name="value">Проверямая строка.</param>
+        /// <param name="propertyName">Имя свойства или объекта, которое подлежит проверке.</param>
+        public static void AssertStringOnPhoneNumber(string value, string propertyName)
+        {
+            string patternOfNumber = @"^(\+7|8)?[\s\-]?\(?\d{3}\)?[\s\-]?\d{3}[\s\-]?\d{2}[\s\-]?\d{2}$";
+            if (!Regex.IsMatch(value, patternOfNumber))
+            {
+                throw new ArgumentOutOfRangeException($"Ошибка в {propertyName}");
+            }
+        }
+
+        /// <summary>
+        /// Проверяет, что строка по формату является электронной почтой.
+        /// </summary>
+        /// <param name="value">Проверямая строка.</param>
+        /// <param name="propertyName">Имя свойства или объекта, которое подлежит проверке.</param>
+        public static void AssertStringOnPhoneEmail(string value, string propertyName)
+        {
+            string patternOfEmail = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+            if (!Regex.IsMatch(value, patternOfEmail))
+            {
+                throw new ArgumentOutOfRangeException($"Ошибка в {propertyName}");
+            }
+        }
+    }
+}
